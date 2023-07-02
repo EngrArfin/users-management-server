62ad2c67d27ed41cb9be1a1beda7bbe8f5eae45b
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-07-02 18:06:24 +0600

    post get data update

---
+++ _step_.js
@@ -0,0 +1,10 @@
+/* 
+1. create a post api on the server site
+2. clint side send data by post
+3. set fetch method inside the fetch option 
+4. option will have tree thinks:  method
+5. option will have headers : content-type: applicration/json
+6. dont forget to send data by JSON.stringify in teh body 
+7. on the server side: don't forget to express json()
+
+*/
--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middleware
+app.use(cors());
+
+
 const users = [
-    {id: 1, name: 'Sabana', email: 'arfinmia1298@gmail.com'},
-    {id: 1, name: 'Saban', email: 'arfinmia129@gmail.com'},
-    {id: 1, name: '', email: 'arfinmia12@gmail.com'},
+    {id: 1, name: 'Arfin', email: 'arfinmia1298@gmail.com'},
+    {id: 2, name: 'Evon', email: 'arfinmia129@gmail.com'},
+    {id: 3, name: 'Shuvo', email: 'arfinmia12@gmail.com'},
 ]
 
 app.get('/', (req, res) =>{
     res.send('Users Management Server is running ');
 });
+app.post('/users', (req, res)=>{
+    res.send(users);
+})
 
 app.get('/users', (req, res) => {
-    res.send(users);
+    console.log('post api hitting ')
+    console.log(req.body);
+
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
+
 })
 
 app.listen(port, () => {
